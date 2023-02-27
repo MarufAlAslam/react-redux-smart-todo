0030486bb4513ae16db90e8621c735c8efb3fca4
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-02-27 12:03:23 +0600

    react initiated, tailwind initiated, header initiated

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+const Main = () => {
+  return (
+    <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+      <Header />
+    </div>
+  );
+};
+
+export default Main;
